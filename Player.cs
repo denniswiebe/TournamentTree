ec88bc3c501a30f14cfd1dc44a52f96107453d15
dennis_wiebe98@yahoo.de <dennis_wiebe98@yahoo.de>
2020-11-03 17:01:01 +0100

    Added more properties to the Player class

--- Player.cs
+++ Player.cs
@@ -9,12 +9,15 @@ namespace TournamentTree
     {
         public string PlayerName { get; set; }
         public int Wins { get; set; }
-        public int Loses { get; set; }
+        public int Ties { get; set; }
+        public int Points { get => Wins * 3 + Ties; }
+        public int GoalDifference { get; set; }
         public int Placement { get; set; }
 
         public Player(string name)
         {
             PlayerName = name;
         }
+
     }
 }
