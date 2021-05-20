b17b2c86bb2b0542355f11d163cacbf9bf939723
denniswiebe <56267483+denniswiebe@users.noreply.github.com>
2021-05-20 13:06:35 +0200

    Player ID und To String Methode für den Player hinzugefügt

--- DataHolderClasses/Player.cs
+++ DataHolderClasses/Player.cs
@@ -8,16 +8,28 @@ namespace TournamentTree
     public class Player
     {
         public string PlayerName { get; set; }
+
+        public int PlayerID { get; set; }
+
         public int Wins { get; set; }
+
         public int Ties { get; set; }
+
         public int Points { get => Wins * 3 + Ties; }
+
         public int GoalDifference { get; set; }
+
         public int Placement { get; set; }
 
-        public Player(string name)
+        public Player(string name, int id)
         {
             PlayerName = name;
+            PlayerID = id;
         }
 
+        public override string ToString()
+        {
+            return PlayerID + ". " + PlayerName;
+        }
     }
 }
