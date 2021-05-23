c257e19779ec7c4970238c8f05339377ff65b1cc
ANTHLD <56266658+ANTHLD@users.noreply.github.com>
2021-05-23 14:42:05 +0200

    Test für GetLetterByNumber Methode
    * TournamentLogTest:
    - Neue Methode für das Testen der GetLetterByNumber-Methode
    - Es wird getestet, ob bei 2 wie erwartet ein B zurückgegeben wird
    - Bei einer Eingabe von 1000, wird in der Methode die Exception geworfen, der Test muss dennoch erfolgreich sein, deswegen das ExpectedException-Attribut über der Methode

---
+++ TournamentTree.UnitTests/TournamentLogTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentTree.UnitTests
+{
+    [TestClass]
+    public class TournamentLogTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void GetLetterByNumberTest()
+        {
+            // Prüfen, ob bei 2 auch ein B kommt
+            Assert.AreEqual(TournamentLog.GetLetterByNumber(2), "B");
+
+            // Methode mit falscher Zahl aufrufen, sodass dort die Exception geworfen wird
+            // Der Test darf nicht schiefgehen
+            TournamentLog.GetLetterByNumber(1000);
+        }
+    }
+}
