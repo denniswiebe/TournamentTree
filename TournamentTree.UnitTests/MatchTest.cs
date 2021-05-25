66a11aab7975b2672b1594a74fd9dc1f0d0feaf7
denniswiebe <56267483+denniswiebe@users.noreply.github.com>
2021-05-25 11:27:02 +0200

    Unit Tests MatchTest

---
+++ TournamentTree.UnitTests/MatchTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TournamentTree.UnitTests
+{
+    [TestClass]
+    public class MatchTest
+    {
+        [TestMethod]
+        public void ChangeHomeAndAway()
+        {
+            Player playerOne = new Player("PlayerOne", 1);
+            Player playerTwo = new Player("PlayerTwo", 2);
+            Match match = new Match(playerOne, playerTwo);
+
+            match.ChangeHomeAndAway();
+
+            Assert.AreEqual(match.PlayerOne, playerTwo);
+            Assert.AreEqual(match.PlayerTwo, playerOne);
+        }
+
+        [TestMethod]
+        public void CheckInputOfMatchTest()
+        {
+            Player playerOne = new Player("PlayerOne", 1);
+            Player playerTwo = new Player("PlayerTwo", 2);
+            Match match = new Match(playerOne, playerTwo);
+
+            bool checkedInputTrue = match.CheckInputOfMatch("4 2");
+            bool checkedInputFalse = match.CheckInputOfMatch("a 1");
+            bool checkedInputFalse2 = match.CheckInputOfMatch("12");
+            bool checkedInputFalse3 = match.CheckInputOfMatch("1 a");
+
+            Assert.AreEqual(true, checkedInputTrue);
+            Assert.AreEqual(false, checkedInputFalse);
+            Assert.AreEqual(false, checkedInputFalse2);
+            Assert.AreEqual(false, checkedInputFalse3);
+        }
+    }
+}
