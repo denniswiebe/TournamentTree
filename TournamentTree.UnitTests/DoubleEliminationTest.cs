668af6af13fe90fa7143d74869185dc5373ced0d
denniswiebe <56267483+denniswiebe@users.noreply.github.com>
2021-05-25 15:03:46 +0200

    Unit Test Double Elimination

---
+++ TournamentTree.UnitTests/DoubleEliminationTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentTree.UnitTests
+{
+
+    [TestClass]
+    public class DoubleEliminationTest
+    {
+        [TestMethod]
+        public void MoveLosingPlayerTest()
+        {
+            Player playerA = new Player("A", 1);
+            Player playerB = new Player("B", 2);
+            Player playerC = new Player("C", 3);
+            Player playerD = new Player("D", 4);
+            List<Player> allPlayers = new List<Player>()
+            {
+                playerA,
+                playerB,
+                playerC,
+                playerD
+            };
+
+            DoubleElimination doubleElimination = new DoubleElimination(allPlayers);
+            List<Player> loserPlayers = new List<Player>()
+            {
+                playerA,
+                playerC
+            };
+
+            doubleElimination.MoveLosingPlayers(loserPlayers);
+
+            Assert.IsTrue(doubleElimination.Winners.Count == 2);
+            Assert.IsTrue(doubleElimination.Losers.Count == 2);
+            Assert.IsTrue(doubleElimination.Losers.Contains(playerA));
+            Assert.IsTrue(doubleElimination.Losers.Contains(playerC));
+            Assert.IsTrue(doubleElimination.Winners.Contains(playerB));
+            Assert.IsTrue(doubleElimination.Winners.Contains(playerD));
+            
+        }
+    }
+}
